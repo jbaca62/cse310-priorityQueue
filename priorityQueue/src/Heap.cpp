d041264ee2d6db9752e3e84eb53dd6fe956be559
jbaca62 <jbaca6@asu.edu>
2018-10-11 14:58:37 -0700

    added initialize

--- priorityQueue/src/Heap.cpp
+++ priorityQueue/src/Heap.cpp
@@ -2,10 +2,15 @@
 
 Heap::Heap()
 {
-    //ctor
+    capacity = 0;
+    size = 0;
+    H = null;
 }
 
-Heap::~Heap()
-{
-    //dtor
+Heap::initialize(int n){
+    Heap newHeap = Heap();
+    newHeap.capacity = n;
+    newHeap.size = 0;
+    newHeap.H = new Element[n];
+    return newHeap;
 }
